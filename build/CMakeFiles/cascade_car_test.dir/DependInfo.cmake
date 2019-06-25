644e8d05634e3800c426b6fa49ad029948bcfae6
陈渊 <2200584313@qq.com>
2019-06-25 11:17:49 +0800

    first integration commit

---
+++ build/CMakeFiles/cascade_car_test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hushoujie/train_car/test/src/cascade_car_test.cpp" "/home/hushoujie/train_car/test/build/CMakeFiles/cascade_car_test.dir/src/cascade_car_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cascade_car_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cascade_car_test.dir/src/cascade_car_test.cpp.o"
+  "../bin/cascade_car_test.pdb"
+  "../bin/cascade_car_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cascade_car_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
